be68d0c0ba74baf2c3b154641ef7a461eb7f8c96
James Lafferty <jameslafferty@Jamess-MBP.hsd1.ca.comcast.net>
2020-02-15 17:19:55 -0800

    feat: implements getName

--- src/getName.ts
+++ src/getName.ts
@@ -1,7 +1,20 @@
+import { continents } from './continents';
+
+const dictionary: Record<string, string> = {};
 /**
  * 
  * @param abbreviation 
  */
-export const getName = (abbreviation: string): string => {
-  return '';
+export const getName = (key: string): string => {
+  if (dictionary[key] == null) {
+    for (const entry of Object.entries(continents)) {
+      const {1: countries} = entry;
+      for (const country of countries) {
+        const { abbr, name, ioc } = country;
+        dictionary[ioc] = name;
+        dictionary[abbr] = name;
+      }
+    }
+  }
+  return dictionary[key];
 };
